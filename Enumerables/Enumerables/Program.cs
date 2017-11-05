845c896f40587dff6b74e95476e29b9ca7bb78b6
KonstantinRupchanski <konstantin.rupchanski@gmail.com>
2017-11-05 13:12:38 +0200

    -----

---
+++ Enumerables/Enumerables/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Enumerables
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string input = "aSd2&5s@1";
+            new WithoutRegex().Run(input);
+            new WithRegex().Run(input);
+        }
+
+
+    }
+
+    public class WithRegex
+    {
+        Regex r = new Regex(@"(?<str>.+?)(?<num>\d+)");
+
+        public void Run(string input)
+        {
+            StringBuilder final = new StringBuilder();
+            var matches = r.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                for (int i = 0; i < Int32.Parse(match.Groups["num"].Value); i++)
+                {
+                    final.Append(match.Groups["str"].Value.ToUpper());
+                }
+            }
+
+            Console.WriteLine(final.ToString());
+        }
+    }
+
+    public class WithoutRegex
+    {
+        StringBuilder final = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder intBuilder = new StringBuilder();
+
+        public void Run(string input)
+        {
+
+            bool lastWasInt = false;
+            foreach (char c in input) //Enumerate string by each character
+            {
+                if (!Char.IsDigit(c)) // check if current character is not a digit
+                {
+                    if (lastWasInt) // reset temporary builders and put result in final
+                    {
+                        lastWasInt = false;
+
+                        PopulateFinal();
+                    }
+
+                    stringBuilder.Append(Char.ToUpper(c));
+                }
+                else
+                {
+                    intBuilder.Append(c);
+                    lastWasInt = true;
+                }
+            }
+            PopulateFinal();
+            Console.WriteLine(final.ToString());
+        }
+
+        void PopulateFinal()
+        {
+            if (stringBuilder.Length == 0 || intBuilder.Length == 0)
+            {
+                return;
+            }
+
+            int count = Int32.Parse(intBuilder.ToString());
+            intBuilder.Clear();
+
+            string stamp = stringBuilder.ToString();
+            stringBuilder.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                final.Append(stamp);
+            }
+        }
+    }
+}
---
+++ MostCommonLetter/MostCommonLetter/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace MostCommonLetter
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.Write("Please enter some text: ");
+
+            var text = Console.ReadLine();
+            var chars = text.ToCharArray()
+                .Where(x => Char.IsLetter(x))
+                .Select(x => Char.ToUpper(x));
+            Console.WriteLine();
+
+            var characterCount = new Dictionary<char, int>();
+
+            foreach (var c in chars)
+            {
+                if (characterCount.ContainsKey(c))
+                {
+                    characterCount[c]+=1;
+                }
+                else
+                {
+                    characterCount[c] = 1;
+                }
+            }
+            Console.WriteLine("Most common letters: ");
+            Console.WriteLine();
+
+            int max = characterCount.Max(x => x.Value);
+
+            foreach (var pair in characterCount.OrderByDescending(x => x.Value).ThenBy(x => x.Key).Take(20))
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value,2:D} {GetBar(pair.Value, max)}");
+            }
+        }
+
+        public static string GetBar(int value, int max, int width = 20, char barChar = '#')
+        {
+            if (value > max) throw new ArgumentException("Max must be greater than Value.");
+            int barCount = (int)Math.Round(((double)width / max) * value);
+            string bar = new String(barChar, barCount);
+            return bar;
+        }
+    }
+}
